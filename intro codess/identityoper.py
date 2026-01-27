509231ec8856383154a6a229ae62df73f478040f
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 21:05:35 +0530

    added

---
+++ intro codess/identityoper.py
@@ -0,0 +1,21 @@
+# identity operators
+ 
+# is - returns true if both variable are the same object 
+
+# is not - returns true if both variables are not the same object 
+
+x = 5
+y = 5
+
+print("if x is y" , x is y)
+print("if x is not y" , x is not y)
+
+# membership operators 
+
+# in - returns true if a sequence with the specified value is present in the object 
+
+# not in - returns true if a sequence with the specified value is not present in the object 
+
+fruits= ["apple","banana", " cherry "]
+print("if banana is present in fruits " , "banana" in fruits )
+print("if mango is not present in fruits " , "mango " not in  fruits )
