e2aa94647289c18b9ec7d4da7bf9f17270773d27
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 18:36:55 +0530

    Initial commit with my Python codes

---
+++ intro codess/code01.py
@@ -0,0 +1,29 @@
+print("hello world ")
+print(5) 
+
+# int - 100, -5 , 256  all the whole number 
+# float - all the decimal number 
+# complex - real part + imaginary part (1 + 2j )
+
+# text data type 
+
+# strings - is a sequence of characters - " ", ' '
+
+# boolean data type - give true or false value 
+
+# sequence data type -
+
+# strings or list or tuple 
+
+# we use list or tuple when we have to store sequence of data 
+
+# list is sequence of integer - [1,2,3,4,5] or ["lisa", "araya","tina"]
+
+# tuple - (1,2,2,3,4,,5,6,6,7,8)  in this duplicates value is also acceptable or present
+
+
+# dictionary data type -
+# key-values pairs 
+
+
+# set data type - unordered collection of unique items 
