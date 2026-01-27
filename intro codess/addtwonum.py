eea335f7317826aaaa2d31314bf34457edb31c5a
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 19:26:14 +0530

    added

---
+++ intro codess/addtwonum.py
@@ -0,0 +1,17 @@
+# taking input --- sum of 2 given numbers 
+
+a = int(input("enter a first number "))
+b = int(input("enter a second number "))
+
+result = a + b
+
+print("the result of the given number of sum is : ",result)
+
+
+# + addition 
+# - subtraction 
+# * multiplication 
+# / division 
+# % modulus use for finding remainders 
+# ** exponentiation use for power 
+# // floor division 
