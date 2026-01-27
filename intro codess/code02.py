d671bc2d5dbea223eb404c5133ace63b39a478bd
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 19:08:16 +0530

    added

---
+++ intro codess/code02.py
@@ -0,0 +1,17 @@
+# ascii
+# A TO Z value is 65 - 90 
+#a to z value is 97 to 122
+# 0-9 value is 48 to 57
+# space - 32
+
+# ord( )function to find ascii value 
+
+char = "a"
+
+print(ord(char))
+
+# chr() function to find character of ascii value 
+
+ascii =67
+
+print(chr(ascii))
