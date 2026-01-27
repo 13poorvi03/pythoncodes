3d3b2c88f0360814a911a0d0b5c099c0317a6fd5
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 19:19:03 +0530

    added

---
+++ intro codess/code03.py
@@ -0,0 +1,12 @@
+# taking input -- ex--- name = input("enter your name ")
+
+name = input("enter your name : ")
+rollno = int(input("enter your roll number : "))
+print(name)  #input always captured as string  it aslo take integer as strings means wo 14 ko bhi as strings hi lega 
+print(rollno)
+
+print(type(rollno))  # using typecasting to convert data type (string to integer data type)
+
+age = input("enter your age : ")
+print(age)
+print(type(age))
