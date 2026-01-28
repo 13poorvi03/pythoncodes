564a09c59fe943b18426bb0fd653cf25e4d3e542
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-28 23:55:38 +0530

    added

---
+++ intro codess/conditional/code05.py
@@ -0,0 +1,8 @@
+#take positive integer input and tell if it is a four digit number or not 
+
+num = int(input("enter a number "))
+
+if num>999 and 9999:
+    print("this is a four digit number")
+else:
+    print("this is not a four digit number")
