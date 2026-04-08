d1c1761f869a3b8b303cbf28ec8b7a751f417cdb
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-08 17:42:44 +0530

    added

---
+++ intro codess/codesss/operator.py
@@ -0,0 +1,60 @@
+a = 76
+b = 15
+print(a+b)
+print(a-b)
+print(a*b)
+
+#  if we divided this it always give ans in float  
+#  so we use flow division by using // so the ans comes in integer 
+s =32
+u = 5
+print(s/u)
+print(s//u)
+
+#  ** use for exponential values for using power 5*5 = 25
+print(5**2)
+print(5**3)
+print(5**100)
+
+# % using for remainder 
+
+print(32%6)
+
+print(12+3/7)
+
+
+# asssignment operators 
+
+s =80
+w = 34
+# compound assignment operators 
+s +=56
+print(s)
+w -=2
+print(w)
+s*=3
+print(s)
+w /=2
+print(w)
+
+
+
+
+
+#  comaprision operators 
+#  == , != , > , < , >= , <=
+ 
+# logical operators
+#  and , or , not 
+
+a = 12
+b = 13
+
+print(a<b)
+print(a != b)
+
+# ascii value 
+print(ord("A"))
+print(ord("a"))
+print(ord("z"))
+print(ord("Z"))
