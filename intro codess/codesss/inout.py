8a09a885b527b691e78857f0bfc009d69f24e70d
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-08 17:17:15 +0530

    added

---
+++ intro codess/codesss/inout.py
@@ -0,0 +1,11 @@
+name = "poorvi"
+age = "23"
+print(name ,age )
+print("hello my name is",name, "and my age is",age)
+
+# f is formatted string 
+print(f"my name is {name} and my age is {age}")
+
+
+uage = input("hello what is your age : ")
+print(uage)
---
+++ intro codess/codesss/typeconversion.py
@@ -0,0 +1,34 @@
+# type conversion 
+# it used to convert one data type to another 
+a =12
+a = str(a)
+print(type(a))
+
+v = "12"
+v = int(v)
+print(v)
+print(type(v))
+
+b = 12
+print(bool(a))
+
+c = "hello"
+print(bool(c))
+
+r = 0
+print(bool(r))
+
+
+# falsy values ---- false, 0, 0.0, " " , [ ] , { } , ( ) it return false
+
+# type of type conversion 
+
+# implict - python automatically converts data one data type to another 
+# ex - a =12 print(a/2)
+
+
+# explicit - we as a user use in build function to convert one data type to another 
+# ex - int() , float() , str()
+ 
+p = 12
+print(p/3)
