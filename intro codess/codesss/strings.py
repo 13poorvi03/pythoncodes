e15f6149c126d17beba27c92c5a56c6286ea8866
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-08 16:37:46 +0530

    added

--- intro codess/codesss/strings.py
+++ intro codess/codesss/strings.py
@@ -1,6 +1,7 @@
 
 # string indexing
 
+# indexing start from [0] it could be negative and positive both 
 
 c= "hello broo what's up"
 
@@ -9,6 +10,25 @@ print(c[2])
 #  //printing from lastt
 
 print(c[-6])
+print(c[6],c[-4])
+
+# ord convvert letter in unique code
+a= "A"
+print(ord(a))  
+
+# chr convert unique code in letter
+b = 65
+print(chr(b))
 
 
+# string slicing
 
+# slicing means cutting out a slice from string and this is also done using index values 
+# we have start ,stop and steps position and keep a note if we use stop at 4 will slice till 3 only 
+
+o = "kiii haal chaal"
+print(o[0:3:1])
+
+# default :: it print to end
+r = "SHER CODER"
+print(r[5::1])
