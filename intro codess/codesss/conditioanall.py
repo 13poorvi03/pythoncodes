c45137370d636537da3581f1cc627e3e2ad7ffe6
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-08 19:03:28 +0530

    added'

---
+++ intro codess/codesss/conditioanall.py
@@ -0,0 +1,29 @@
+a = 13
+
+if a <10:
+  print(" I will do task A")
+
+else:
+  print("I will do task B")
+
+#   ///////////////////////////////////////////////
+
+  money = int(input("please provide me the money : - "))
+  if money > 40:
+    print("i will buy chips coz money is : ",money)
+
+  else:
+    print("i will buy chocolate coz money is : ",money)
+
+#   ///////////////////////////////////////////////
+
+marks = int(input("enter the marks : "))
+
+if marks > 80:
+  print("the student got A1")
+
+elif marks >60:
+  print(" the student got C ")
+
+else :
+   print("student is an average performer ")
