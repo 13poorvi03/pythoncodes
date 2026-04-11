756c4cd5dcdbab1fc07914aa2a0508285e3f2aad
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-12 00:22:08 +0530

    added

---
+++ intro codess/codesss/stringssloop.py
@@ -0,0 +1,18 @@
+
+# iritating using the index values 
+
+a = "poorvisharma"
+
+for i in range(12):
+    print(a[i])
+
+
+s = "talkingtom"
+print(len(s))
+for i in range(len(s)):
+    print(s[i])    
+
+# direct access to char 
+k = "hello what's up guyz"
+for i in k:
+    print(i)
