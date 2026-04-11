20bcba2bc4e1614a7c74e337aeae7b909108192b
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-12 00:08:53 +0530

    added

---
+++ intro codess/codesss/loop2.py
@@ -0,0 +1,17 @@
+
+# print in reserve order
+
+for i in range(20,1,-1):
+    print(i)
+
+# print a table of 5 
+
+for i in range(5,51,5):
+    print(i)
+
+
+# take an input and print table 
+
+n = int(input("which table u want ? "))
+for i in range(n,n*11,n):
+    print(i)
