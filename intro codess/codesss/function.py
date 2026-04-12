5c3c6a642b824088e8c467fe4246954d60f042a3
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-12 20:03:56 +0530

    added

---
+++ intro codess/codesss/function.py
@@ -0,0 +1,19 @@
+
+# its a block of reusable code that we create whenever we want to call it then only it will work 
+
+# def hello():
+#     print("this is a hello fucntion ")
+
+# hello()
+
+
+
+# create a function for sum of given integer 
+
+def sum(a,b):
+    print(f"the sum of your numbers is {a+b}")
+
+sum(12,3)   
+sum(69,88)
+
+  
--- intro codess/codesss/practise.01.py
+++ intro codess/codesss/practise.01.py
@@ -1,134 +1,134 @@
 # accept an integer and print hello world n times 
 
-# n = int(input("enter the number :  "))
-# for i in range(n):
-#     print("hello world ")
+n = int(input("enter the number :  "))
+for i in range(n):
+    print("hello world ")
 
 
 # print natural number up to n 
 
-# n = int(input("enter the number : "))
-# for i in range(n):
-#     print(i)
+n = int(input("enter the number : "))
+for i in range(n):
+    print(i)
 
 
 #  reverse for loop print n to 1
 
-# n = int(input("enter the number : "))
-# for i in range(n,0,-1):
-#     print(i)
+n = int(input("enter the number : "))
+for i in range(n,0,-1):
+    print(i)
 
 
 # sum up to n terms 
 
-# n = int(input("enter the number : "))
-# sum =0
-# for i in range(1,n+1):
-#     sum += i
-#     print(f"your sum is {sum}")
+n = int(input("enter the number : "))
+sum =0
+for i in range(1,n+1):
+    sum += i
+    print(f"your sum is {sum}")
 
 
 
 # factorial of a number 
 
-# n = int(input("enter the number : "))
-# factorial = 1
-# for i in range(1,n+1):
-#     factorial *= i
-#     print(f"your factorial is {factorial}")
+n = int(input("enter the number : "))
+factorial = 1
+for i in range(1,n+1):
+    factorial *= i
+    print(f"your factorial is {factorial}")
 
 
 # print the sum of all even and odd numbers in a range separetly 
 
-# n = int(input("Enter the number: "))
-# even = 0 
-# odd = 0
+n = int(input("Enter the number: "))
+even = 0 
+odd = 0
 
-# for i in range(1, n+1):
-#     if i % 2 == 0:
-#         even += i
-#     else:
-#         odd += i  
+for i in range(1, n+1):
+    if i % 2 == 0:
+        even += i
+    else:
+        odd += i  
 
-# print("Sum of even numbers:", even)
-# print("Sum of odd numbers:", odd)
+print("Sum of even numbers:", even)
+print("Sum of odd numbers:", odd)
 
 
 
 # print all the factors of a number 
 
-# n = int(input("enter the number : "))
-# for i in range(1,n+1):
-#     if n%i == 0:
-#         print(i)
+n = int(input("enter the number : "))
+for i in range(1,n+1):
+    if n%i == 0:
+        print(i)
 
 
 
 #  accept a number and check 
 
-# n = int(input("Enter the number: "))
-# sum = 0
+n = int(input("Enter the number: "))
+sum = 0
 
-# for i in range(1, n):
-#     if n % i == 0:
-#         sum += i
+for i in range(1, n):
+    if n % i == 0:
+        sum += i
 
-# if sum == n:
-#     print("This is a perfect number:", sum)
-# else:
-#     print("This is not a perfect number")
+if sum == n:
+    print("This is a perfect number:", sum)
+else:
+    print("This is not a perfect number")
 
 
 
 
 # prime number 
 
-# n = int(input("enter the number : "))
-# count = 0
-# for i in range(1, n+1):
-#     if n%i==0:
-#         count+=1
+n = int(input("enter the number : "))
+count = 0
+for i in range(1, n+1):
+    if n%i==0:
+        count+=1
 
-# if count == 2:
-#     print("your number is prime ")
-# else:
-#     print("your number is not prime")         
+if count == 2:
+    print("your number is prime ")
+else:
+    print("your number is not prime")         
 
 
 
 # reverse strings 
 
-# p = "Helloo jiii ki haal chaal "
-# l = "sab theek hai  "
+p = "Helloo jiii ki haal chaal "
+l = "sab theek hai  "
 
-# for i in range(len(p)-1,-1,-1):
-#     print(p[i])
+for i in range(len(p)-1,-1,-1):
+    print(p[i])
 
-# print(p+l)
-# print(p[::-1])
+print(p+l)
+print(p[::-1])
 
 
 
 # palindrome
 
-# h = "Appp kon ho bhai "
-# b = ""
+h = "Appp kon ho bhai "
+b = ""
 
-# for i in range(len(h)-1, -1, -1):
-#     b = b + h[i]
+for i in range(len(h)-1, -1, -1):
+    b = b + h[i]
 
-# if b == h:
-#     print("Your string is palindrome")
-# else:
-#     print("Your string is not palindrome")
-
-
+if b == h:
+    print("Your string is palindrome")
+else:
+    print("Your string is not palindrome")
 
 
-# h = "madam"
-# if h == h[::-1]:
-#     print("Palindrome")
-# else:
-#     print("Not palindrome")
+
+
+h = "madam"
+if h == h[::-1]:
+    print("Palindrome")
+else:
+    print("Not palindrome")
 
 
