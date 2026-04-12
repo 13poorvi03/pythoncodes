b7dd1740f1aef1b423e81a9f6e15324f4ce6cc04
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-12 11:13:54 +0530

    added

---
+++ intro codess/codesss/practise.01.py
@@ -0,0 +1,67 @@
+# accept an integer and print hello world n times 
+
+# n = int(input("enter the number :  "))
+# for i in range(n):
+#     print("hello world ")
+
+
+# print natural number up to n 
+
+# n = int(input("enter the number : "))
+# for i in range(n):
+#     print(i)
+
+
+#  reverse for loop print n to 1
+
+# n = int(input("enter the number : "))
+# for i in range(n,0,-1):
+#     print(i)
+
+
+# sum up to n terms 
+
+# n = int(input("enter the number : "))
+# sum =0
+# for i in range(1,n+1):
+#     sum += i
+#     print(f"your sum is {sum}")
+
+
+
+# factorial of a number 
+
+# n = int(input("enter the number : "))
+# factorial = 1
+# for i in range(1,n+1):
+#     factorial *= i
+#     print(f"your factorial is {factorial}")
+
+
+# print the sum of all even and odd numbers in a range separetly 
+
+# n = int(input("Enter the number: "))
+# even = 0 
+# odd = 0
+
+# for i in range(1, n+1):
+#     if i % 2 == 0:
+#         even += i
+#     else:
+#         odd += i  
+
+# print("Sum of even numbers:", even)
+# print("Sum of odd numbers:", odd)
+
+
+
+# print all the factors of a number 
+
+# n = int(input("enter the number : "))
+# for i in range(1,n+1):
+#     if n%i == 0:
+#         print(i)
+
+
+
+#  accept a number and check 
