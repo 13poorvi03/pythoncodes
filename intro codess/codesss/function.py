8d92d2da5c05412d75a1637bcbd0ce9c356daaca
13poorvi03 <poorvisharma0306@gmail.com>
2026-04-12 20:18:31 +0530

    added

--- intro codess/codesss/function.py
+++ intro codess/codesss/function.py
@@ -16,4 +16,34 @@ def sum(a,b):
 sum(12,3)   
 sum(69,88)
 
-  
+#   types of parameter 
+
+# positional arguments 
+
+def sub(a,b):
+    print(f"the subtraction of two number : {a-b}")
+
+sub(19,2)
+sub(15,5)
+
+
+# default arguments 
+
+def introduce(name,age):
+    print(f"i am {name} and i am {age} years old .")
+
+introduce("poorvi",20)   
+
+
+# keyword argumnets 
+
+def greet(uname,agee):
+    print(f"your name is {uname} and your age is {agee}")
+
+greet (agee = 22 , uname = "akarsh")
+
+# use of return 
+def gud():
+    return "good morning bro"
+
+print(gud())
