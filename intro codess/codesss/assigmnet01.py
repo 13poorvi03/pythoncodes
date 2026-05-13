ef1b71da011fb531813e4a7419f807192e3a0d05
13poorvi03 <poorvisharma0306@gmail.com>
2026-05-13 23:24:00 +0530

    added

--- intro codess/codesss/assigmnet01.py
+++ intro codess/codesss/assigmnet01.py
@@ -83,3 +83,6 @@ p = "Coding For All"
 print(p.rfind('l'))
 
 # Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
